e82440f46823c72dba20a4e850bbc576770553ab
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-24 15:20:42 +0800

    Add board game model

---
+++ models/BoardGameModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class BoardGame {
+    public int $GameID;
+    public string $GameName;
+    public string $GameDescription;
+    public int $QuantityAvailable;
+    public string $GameCategory;
+    public string $GameStatus;
+
+    function __construct() {}
+}
+
+?>
