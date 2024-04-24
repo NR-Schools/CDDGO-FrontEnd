e6342be8dd28839b147932a67dcd4c1c1f7f86d8
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-24 15:47:48 +0800

    Add event model

---
+++ models/EventModel.php
@@ -0,0 +1,14 @@
+<?php
+
+class Event {
+    public int $EventID;
+    public string $EventName;
+    public string $EventDescription;
+    public string $EventDate;
+    public string $EventLocation;
+    public string $DatePosted;
+
+    function __construct() {}
+}
+
+?>
