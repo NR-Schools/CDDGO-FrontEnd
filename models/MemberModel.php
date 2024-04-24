410f7c4ff2d902b40c359d7560c6271760c41883
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-24 19:34:35 +0800

    Add member model and service

---
+++ models/MemberModel.php
@@ -0,0 +1,9 @@
+<?php
+
+class Member {
+    public Student $student; // This also fetches the student details; student id is member id
+    public string $Position;
+    public int $YearJoined;
+}
+
+?>
