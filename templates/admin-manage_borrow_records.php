eb8e3fc9a943f393ac648017ae41b3acc79f3db4
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-26 14:50:20 +0800

    Add Manage Borrow Records View of Admin

---
+++ templates/admin-manage_borrow_records.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="../css/admin-manage_borrow_records.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- Include Header -->
+    <?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php";
+    ?>
+
+    <!-- Start Body -->
+    <div class="main-body">
+        <p>ACTIVE TRANSACTIONS</p>
+        <hr />
+
+        <!-- Loop This Shit -->
+        <div class="borrow-record-entry">
+            <div>
+                <span>1</span>
+                <span>2021103749</span>
+                <span>Manigga</span>
+                <span>April 30, 2024</span>
+                <span>P 300</span>
+            </div>
+            <div>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+
+        <div class="borrow-record-entry">
+            <div>
+                <span>2</span>
+                <span>2021103749</span>
+                <span>Maniggssssssa</span>
+                <span>April 30, 2024</span>
+                <span>P 300</span>
+            </div>
+            <div>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
