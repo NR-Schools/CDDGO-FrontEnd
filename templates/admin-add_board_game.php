2b9b88d5f7dc846540c3d50fb77816d1cb388161
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-04-29 21:33:14 +0800

    accept input in addboardgame

--- templates/admin-add_board_game.php
+++ templates/admin-add_board_game.php
@@ -20,11 +20,52 @@
     ?>
 
     <!-- Front End start -->
-    <form action = >
+    <form action="admin-add_board_game.php" method="POST" enctype="multipart/form-data">
+        <!--Board Game Name-->
+        <label for="game_name">Board Game Name</label>
+        <input type="text" name="game_name" id="game_name">
 
+        <!--Board Game Image-->
+        <label for="game_img">Board Game Image</label>
+        <input type="file" id="game_img" name="game_img" class="form-control" onchange="onFileSelected(event)" style="height: 45px;" accept="image/*">
+
+        <!--Description-->
+        <label for="game_desc">Description</label><br>
+        <textarea type="text" name="description" id="description" required class="form-control" style="height: 300px;" ></textarea>
+
+        <!--Category-->
+        <label for="game_category">Category</label>
+        <input type="text" name="game_category" id="game_category">
+
+        <!--Quantity Available-->
+        <label for="quantity_avail">Quantity Available</label>
+        <input type="number" name="quantity_avail" id="quantity_avail">
+
+        <!--Submit Button-->
+        <input type="submit">
     </form>
 
     <!-- Backend Start -->
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $game_image = file_get_contents($_FILES['game_img']['tmp_name']); //event image
+        $image_encoded = base64_encode($game_image);
+
+        $boardgame = new BoardGame();
+
+        $boardgame->GameName = $_POST['game_name'];
+        $boardgame->GameDescription = $_POST['description'];
+        $boardgame->GameImage = $image_encoded;
+        $boardgame->QuantityAvailable = $_POST['quantity_avail'];
+        $boardgame->GameCategory = $_POST['game_category'];
+        #
+
+        
+
+        BoardGameService::addNewBoardGame($boardgame);
+    }
+    ?>
 
     
     
