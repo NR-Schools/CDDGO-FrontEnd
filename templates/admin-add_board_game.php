02dbf3475ccbd802e9bac5bdc028273d28fcca8a
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-04-29 19:11:41 +0800

    Update admin-add_board_game.php

--- templates/admin-add_board_game.php
+++ templates/admin-add_board_game.php
@@ -6,10 +6,9 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add Board Game</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Add Board Game</title>
 </head>
 <body>
     <!-- Include Header and Footer-->
@@ -17,6 +16,9 @@
         require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php"; 
         require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
     ?>
+
+    <!-- Content start -->
+    
     
 </body>
 </html>
