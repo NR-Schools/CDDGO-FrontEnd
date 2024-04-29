1ada39b641867ea8d3759cb50ecf84bdd47584f6
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-04-29 14:15:17 +0800

    Add EventService

---
+++ services/EventService.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/repositories/EventRepository.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/models/EventModel.php";
+
+class EventService {
+    static function addEvent(Event $event): bool
+    {
+        return EventRepository::addNewEvent($event);
+    }
+
+    static function getAllEvents(): array
+    {
+        return EventRepository::getAllEvents();
+    }
+
+    static function updateEvent(Event $event): bool
+    {
+        return EventRepository::updateEvent($event);
+    }
+}
+
+?>
