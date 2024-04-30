e041650b7ee34e58253891ff4c82f4b81dc8c94b
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-05-01 00:58:27 +0800

    admin add-board-game frontend

--- templates/admin-edit_board_game.php
+++ templates/admin-edit_board_game.php
@@ -1,6 +1,11 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . "/services/BoardGameService.php");
     require_once($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
+
+    if (!AuthGuard::guard_route(Role::ADMIN)) {
+        // Return to root
+        // header("Location: /");
+    }
 ?>
 
 <!DOCTYPE html>
