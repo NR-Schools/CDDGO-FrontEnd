756e816d2b44930aa6d0d0bcd815429a0c6fd854
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-04-30 14:08:17 +0800

    updated add-board-games, template on manage-board-games

--- templates/admin-add_board_game.php
+++ templates/admin-add_board_game.php
@@ -20,6 +20,9 @@
     ?>
 
     <!-- Front End start -->
+    <div class="add-board-title">
+        ADD BOARD GAME
+    </div>
     <form action="admin-add_board_game.php" method="POST" enctype="multipart/form-data">
         <!--Board Game Name-->
         <label for="game_name">Board Game Name</label>
@@ -31,28 +34,64 @@
 
         <!--Description-->
         <label for="game_desc">Description</label><br>
-        <textarea type="text" name="description" id="description" required class="form-control" style="height: 300px;" ></textarea>
+        <textarea type="text" name="description" id="description" class="form-control" style="height: 200px;" ></textarea>
 
         <!--Category-->
         <label for="game_category">Category</label>
-        <input type="text" name="game_category" id="game_category">
+        <select name="game_category">
+            <option value="No Category">Select Category</option>
+            <option value="Abstract Strategy">Abstract Strategy</option>
+            <option value="Area Control">Area Control</option>
+            <option value="Campaign">Campaign</option>
+            <option value="City Building">City Building</option>
+            <option value="Cooperative">Cooperative</option>
+            <option value="Deck Building">Deck Building</option>
+            <option value="Deduction">Deduction</option>
+            <option value="Dexterity">Dexterity</option>
+            <option value="Dungeon Crawler">Dungeon Crawler</option>
+            <option value="Economic">Economic</option>
+            <option value="Family">Family</option>
+            <option value="Fighting">Fighting</option>
+            <option value="Hand Management">Hand Management</option>
+            <option value="Kid">Kid</option>
+            <option value="Limited Communication">Limited Communication</option>
+            <option value="Party">Party</option>
+            <option value="Pick-Up and Deliver">Pick-Up and Deliver</option>
+            <option value="Programming">Programming</option>
+            <option value="Set Collection">Set Collection</option>
+            <option value="Storytelling">Storytelling</option>
+            <option value="Tower Defense">Tower Defense</option>
+            <option value="War">War</option>
+            <option value="Word">Word</option>
+            <option value="Worker Placement">Worker Placement</option>
+        </select>
+
 
         <!--Quantity Available-->
         <label for="quantity_avail">Quantity Available</label>
         <input type="number" name="quantity_avail" id="quantity_avail">
 
         <!--Submit Button-->
-        <input type="submit">
+        <button type="submit">Add Board Game</button>
     </form>
+
+            <!--Cancel Button-->
+    <a href="admin-manage_board_games.php">
+        <button>Cancel</button>
+    </a>
 
     <!-- Backend Start -->
     <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
+        //image
         $game_image = file_get_contents($_FILES['game_img']['tmp_name']); //event image
         $image_encoded = base64_encode($game_image);
+
+        //status
         $status = "Available"; //by default available
 
+        //create board games 
         $boardgame = new BoardGame();
 
         $boardgame->GameName = $_POST['game_name'];
@@ -62,7 +101,23 @@
         $boardgame->GameCategory = $_POST['game_category'];
         $boardgame->GameStatus = $status;
         
-        BoardGameService::addNewBoardGame($boardgame);
+        try{
+            BoardGameService::addNewBoardGame($boardgame);
+
+            echo "<script> alert('Board Game Added');
+            document.location.href = 'admin-manage_board_games.php';
+            </script>";
+        }
+
+        catch (PDOExceptionException $e){
+            echo $e->getMessage();
+            echo "<script>
+            alert('Failed to add BoardGame');
+            document.location.href = 'admin-add_board_game.php';
+            </script>
+            ";
+
+        }
     }
     ?>
 
