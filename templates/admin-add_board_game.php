8ccf3d0ee9b647bec8f2f8a4f931662e68e796c3
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-04-30 22:19:53 +0800

    Update admin-add_board_game.php

--- templates/admin-add_board_game.php
+++ templates/admin-add_board_game.php
@@ -11,6 +11,14 @@
     <title>Add Board Game</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .add-board-title 
+        {
+            font-size: 60px;
+            border-bottom: solid black;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <!-- Include Header and Footer-->
