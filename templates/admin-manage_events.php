6b1f9965428e9b54d953b2cb9bf57d08f4a92597
PiiDeeGy <angelemnlcruz@gmail.com>
2024-05-01 09:50:46 +0800

    frontend complete for inquiries both admin and user
    tangina mo ngb

--- templates/admin-manage_events.php
+++ templates/admin-manage_events.php
@@ -4,7 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/guards/AuthGuard.php';
 
 if (!AuthGuard::guard_route(Role::ADMIN)) {
     // Return to root
-    header("Location: /");
+    // header("Location: /");
 }
 
 ?>
