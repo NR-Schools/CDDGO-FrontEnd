413c2306a36fa693cecc08186a94a2b53548aebb
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-05-01 17:46:30 +0800

    Add backend connection for sign up

--- templates/sign-up.php
+++ templates/sign-up.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Sign Up</title>
 </head>
 <body>
     <?php 
@@ -21,7 +21,7 @@
     ?>
 
     <div class="main-container">
-        <div class="sign-up-container">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="sign-up-container">
             <div class="sign-up-title">
                 SIGN-UP ACCOUNT
             </div>
@@ -31,52 +31,75 @@
                 <div class="content-container">
                     <div>
                         <label class="label-styling" for="">First Name</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="text" name="fname" id="fname">
                     </div>
                     <div>
                         <label class="label-styling" for="">Last Name</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="text" name="lname" id="lname">
                     </div>
                     <div>
                         <label class="label-styling" for="">Email</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="text" name="email" id="email">
                     </div>
                 </div>
             </div>
-
             <div class="school-details">
                 <div class="form-title">School Details</div>
                 <div class="divider"></div>
                 <div class="content-container">
                     <div>
                         <label class="label-styling" for="">Student Number</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="text" name="studentNumber" id="studentNumber">
                     </div>
                     <div>
                         <label class="label-styling"f or="">Progam</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="text" name="program" id="program">
                     </div>
                 </div>
             </div>
-
             <div class="account-details">
                 <div class="form-title">Account Details</div>
                 <div class="divider"></div>
                 <div class="content-container">
                     <div>
                         <label class="label-styling" for="">Username</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="text" name="username" id="username">
                     </div>
                     <div>
                         <label class="label-styling" for="">Password</label>
-                        <input class="input-styling" type="text">
+                        <input class="input-styling" type="password" name="password" id="password">
                     </div>
                 </div>
             </div>
             <div class="button-container">
-                <button class="button-styling">SIGN-UP</button>
+                <input type="submit" class="button-styling" value="SIGN-UP">
             </div>
-        </div>
+        </form>
     </div>
 </body>
 </html>
+
+
+<?php
+
+// When trying to sign up, load the html first before checking sign up
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    $student = new Student();
+    $student->FirstName = $_POST['fname'];
+    $student->LastName = $_POST['lname'];
+    $student->Email = $_POST['email'];
+    $student->StudNo = $_POST['studentNumber'];
+    $student->Program = $_POST['program'];
+    $student->Password = $_POST['password'];
+
+
+    // Save Student
+    AuthService::signup($student);
+
+    // Redirect to sign in page
+    header("Location: /sign-in.php");
+}
+
+?>
