aecac3964ec439c77810f0021a5a3795fd90f106
HansGan <90506270+HansGan@users.noreply.github.com>
2024-05-01 15:18:42 +0800

    About-Us-FrontEnd-Finished

--- templates/about-us.php
+++ templates/about-us.php
@@ -1,11 +1,123 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/about-us.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
     <title>MTG Board Game Rental System</title>
 </head>
 <body>
+    <?php
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php";
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
+    ?>
+    <div class="main-container">
+        <div class="about-container">
+            <div class="title-styling">
+                THE ORGANIZATION
+            </div>
+            <div class="divider"></div>
+            <div class="org-content-container">
+                <div class="content-styling">
+                    Mapúa Tabletop Gamers (MTG) began as a student club with the goal of serving the recreational needs of its student body. MTG aims to provide a place where people can unwind and have fun away from the demanding academics of university life. Game nights, tournaments, and enlightening workshops are just a few of the planned events that MTG hosts to encourage social and community service among its members.
+                </div>
+                <div class="content-styling">
+                MTG fosters a sense of community among the school population while providing a hub for students looking for a break from their academic obligations thanks to its wide selection of games and welcoming atmosphere.
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="mission-vision-content">
+                <div class="title-styling">
+                    MISSION
+                </div>
+                <div class="title-styling">
+                    VISION
+                </div>
+                <div class="content-styling-mission">
+                    Mapúa Tabletop Gamers shall become an expansive community of gamers and game enthusiasts joined by their common love for games and recreation.
+                </div>
+                <div class="content-styling-vision">
+                    To realize this vision, Mapúa Tabletop Gamers aims to be an open community for people who have interests in games, and to increase awareness by sharing knowledge of board games and video games, all while carrying the name of Mapúa Tabletop Gamers in-order to reach fellow enthusiasts, and aspiring gamers alike.
+                </div>
+            </div>
+            <div class="title-styling">THE OFFICERS</div>
+            <div class="divider"></div>
+            <div class="officer-container">
+                <div class="officer-content">
+                    <div>
+                        <img class="img-styling" src="../assets/img-placeholder.png" alt="">
+                    </div>
+                    <div class="officer-info">
+                        <div class="officer-name">
+                            OFFICERNAME
+                        </div>
+                        <div class="info">Position</div>
+                        <div class="contact">
+                            Contact Details
+                        </div>
+                        <div class="info">Email</div>
+                    </div>
+                </div>
+
+                <div class="officer-content">
+                    <div>
+                        <img class="img-styling" src="../assets/img-placeholder.png" alt="">
+                    </div>
+                    <div class="officer-info">
+                        <div class="officer-name">
+                            OFFICERNAME
+                        </div>
+                        <div class="info">Position</div>
+                        <div class="contact">
+                            Contact Details
+                        </div>
+                        <div class="info">Email</div>
+                    </div>
+                </div>
+
+                <div class="officer-content">
+                    <div>
+                        <img class="img-styling" src="../assets/img-placeholder.png" alt="">
+                    </div>
+                    <div class="officer-info">
+                        <div class="officer-name">
+                            OFFICERNAME
+                        </div>
+                        <div class="info">Position</div>
+                        <div class="contact">
+                            Contact Details
+                        </div>
+                        <div class="info">Email</div>
+                    </div>
+                </div>
+
+                <div class="officer-content">
+                    <div>
+                        <img class="img-styling" src="../assets/img-placeholder.png" alt="">
+                    </div>
+                    <div class="officer-info">
+                        <div class="officer-name">
+                            OFFICERNAME
+                        </div>
+                        <div class="info">Position</div>
+                        <div class="contact">
+                            Contact Details
+                        </div>
+                        <div class="info">Email</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
     
 </body>
 </html>
