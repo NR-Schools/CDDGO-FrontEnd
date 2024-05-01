960f0ff557aa201061542cf58534d83a912f8ff4
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-05-01 17:46:50 +0800

    Change Doc Title for sign in

--- templates/sign-in.php
+++ templates/sign-in.php
@@ -13,7 +13,7 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300..800;1,300..800&display=swap"
         rel="stylesheet">
-    <title>Document</title>
+    <title>Sign In</title>
 </head>
 
 <body>
