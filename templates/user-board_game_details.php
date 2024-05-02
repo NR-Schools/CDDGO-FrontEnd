6f3d5ac05508d915c595b837e66e8f27a31d14ea
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-05-02 16:14:31 +0800

    initial board game details working

--- templates/user-board_game_details.php
+++ templates/user-board_game_details.php
@@ -1,6 +1,19 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . "/services/BoardGameService.php");
     require_once($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
+
+    //get gameID
+    if(isset($_GET["gameId"]))
+    {
+        $gameID = $_GET["gameId"];
+
+        $game = BoardGameService::getBoardGameById($gameID);
+        if ($game == null)
+        {
+            echo "No game.";
+        }
+        
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,5 +29,25 @@
             require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php"; 
             require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
         ?>
+
+        <!--Content Start-->
+        <div class="content-container">
+            <?php
+                        // Output events for the found game
+                        echo '<div class="game-card" style="padding:0">';
+                        echo '<img class="game-card-pic" style="height:300px;min-width:300" src="data:image/' . pathinfo($game->GameImage, PATHINFO_EXTENSION) . ';base64,' . $game->GameImage . '" id="game_image">';
+                        echo '<div class="game-card-body">';
+                        echo '<h4 class="game-card-title">' . $game->GameName . '</h4>';
+                        echo '<p class="game-card-text">' . $game->GameCategory . '</p>';
+                        echo '<p class="game-card-text"> Quantity Available: ' . $game->QuantityAvailable . '</p>';
+                        echo '<p class="game-card-text"> Overview: ' . $game->GameDescription . '</p>';
+                        echo '<a href="/templates/user-board_game_details.php?gameId=' . $game->GameID . '" class="game-btn">Rent</a>';
+                        echo '<a href="/templates/user-board_game_details.php?gameId=' . $game->GameID . '" class="game-btn">Reserve</a>';
+                        echo '<a href="/templates/user-board_game_details.php?gameId=' . $game->GameID . '" class="game-btn">Back</a>';
+                        echo '</div>';
+                        echo '</div>';
+            ?>
+        </div>
+        
 </body>
 </html>
