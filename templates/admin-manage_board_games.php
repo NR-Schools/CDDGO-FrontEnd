a8c658261152763a1aaf3e43120acc07a45c496b
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-05-05 13:30:59 +0800

    manage board game UI updated

--- templates/admin-manage_board_games.php
+++ templates/admin-manage_board_games.php
@@ -15,44 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Board Games</title>
     <link type="text/css" rel="stylesheet" href="../css/admin-manage_board_games.css">
-    <style>
-        .title-container 
-        {
-            overflow: hidden;
-        }
 
-        .title
-        {
-            display: inline-block; /* Display elements side by side */
-            vertical-align: top; /* Align them to the top */
-            margin-right: 800px;
-        }
-
-        .add-button
-        {
-            display: inline-block; /* Display elements side by side */
-            vertical-align: top; /* Align them to the top */
-            float: right;
-        }
-
-        .add-btn
-        {
-            display: inline-block;
-            background-color: #9e0671; 
-            color: white;
-            border-radius: 8px;
-            border: solid #9e0671;
-            padding: 10px;
-            transition:background-color 0.5s, border-style 0.5s, color 0.5s;
-        }
-
-        .add-btn:hover
-        {
-            background-color: #ffffff;
-            color: #9e0671;
-        }
-
-    </style>
 </head>
 <body>
     <!-- Include Header and Footer-->
@@ -84,14 +47,14 @@
 
                 //Output events
                 echo '<div class="game-card" style="padding:0">';
-                echo '<img class="game-card-pic" style="height:300px;min-width:300" src="data:image/' . pathinfo($game->GameImage, PATHINFO_EXTENSION) . ';base64,' . $game->GameImage . '" id="event_image">';
-                echo '<div class="game-card-body">';
-                echo '<h4 class="game-card-title">' . $game->GameName . '</h4>';
-                echo '<p class="game-card-text">' .$game->GameDescription . '</p>';
-                echo '<p class="game-card-text">' .$game->GameCategory . '</p>';
-                echo '<p class="game-card-text">' .$game->QuantityAvailable. '</p>';
-                echo '<a href="/templates/admin-edit_board_game.php?gameId=' . $game->GameID . '" class="game-btn">Edit</a>';
-                echo '</div>';
+                echo    '<img class="game-card-pic" style="height:300px;min-width:300" src="data:image/' . pathinfo($game->GameImage, PATHINFO_EXTENSION) . ';base64,' . $game->GameImage . '" id="event_image">';
+                echo    '<div class="game-card-body">';
+                echo        '<h4 class="game-card-title">' . $game->GameName . '</h4>';
+                echo        '<p class="game-card-desc">' .$game->GameDescription . '</p>';
+                echo        '<p class="game-card-text">' .$game->GameCategory . '</p>';
+                echo        '<p class="game-card-text">' .$game->QuantityAvailable. '</p>';
+                echo    '</div>';
+                echo    '<a class="button-container" href="/templates/admin-edit_board_game.php?gameId=' . $game->GameID . '"><button class="game-btn">Edit</button></a>';  
                 echo '</div>';
             }
         }
