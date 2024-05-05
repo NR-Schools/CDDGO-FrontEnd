4aec815bf5fefdf1ac43fd68e83a97a1b768f566
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-05-05 20:44:30 +0800

    Change SSE to Poll-based

---
+++ endpoints/notification_endpoint.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/services/AuthService.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/services/NotificationService.php';
+
+class NotificationDTO
+{
+    public bool $status;
+    public string $error_msg;
+    public array $notifications;
+    
+    function __construct() {}
+
+    static function create(): NotificationDTO
+    {
+        return new NotificationDTO();
+    }
+
+    function setStatus(bool $status): NotificationDTO
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    function setErrorMessage(string $error_msg): NotificationDTO
+    {
+        $this->error_msg = $error_msg;
+        return $this;
+    }
+
+    function setNotifications(array $notifications): NotificationDTO
+    {
+        $this->notifications = $notifications;
+        return $this;
+    }
+
+    function build(): string
+    {
+        return json_encode($this);
+    }
+}
+
+
+// Check if signed-in
+[$email, $role] = AuthService::getCurrentlyLoggedIn();
+if ($email == null)
+{
+    echo NotificationDTO::create()
+                        ->setStatus(false)
+                        ->setErrorMessage("No Logged In User")
+                        ->setNotifications([])
+                        ->build();
+    exit();
+}
+
+// Return Notifications
+echo NotificationDTO::create()
+                        ->setStatus(true)
+                        ->setErrorMessage("None")
+                        ->setNotifications(
+                            NotificationService::getAllNotificationsByEmail($email)
+                        )
+                        ->build();
+
+?>
