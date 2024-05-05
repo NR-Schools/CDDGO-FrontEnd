d21489130f7ee6a78d8b640fe6b6d3900dae24a6
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-05-05 15:36:48 +0800

    Connect admin reply to inquiry to "backend"

--- templates/admin-reply_to_inquiry.php
+++ templates/admin-reply_to_inquiry.php
@@ -1,35 +1,62 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/services/BoardGameService.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/services/InquiryService.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
 
-    if (!AuthGuard::guard_route(Role::ADMIN)) {
-        // Return to root
-        // header("Location: /");
-    }
-    #Include Header and Footer
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php"; 
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
+if (!AuthGuard::guard_route(Role::ADMIN)) {
+    // Return to root
+    // header("Location: /");
+}
+#Include Header and Footer
+require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
 ?>
+
+
+
+<?php
+
+$inquiryId = $_GET['inquiryId'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $inquiryResponse = new InquiryResponse();
+    $inquiryResponse->RefInquiryID = $inquiryId;
+    $inquiryResponse->ResponseText = $_POST['replyText'];
+    InquiryService::adminReplyToInquiry($inquiryResponse);
+}
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Inquiry Reply</title>
-            <link type="text/css" rel="stylesheet" href="../css/admin-reply_to_inquiry.css">
-        </head>
-    <body>
-        <!-- Frontend Start -->
-        <div class="box">
-            <h3>Reply to Inquiry</h3>
-            <textarea placeholder="Start writing reply..."></textarea>
-                <div>
-                    <button class="btn reply">Reply to Inquiry</button>
-                    <button class="btn cancel">Cancel</button>
-                </div>
-        </div>
-        <!-- Backend Start -->
 
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inquiry Reply</title>
+    <link type="text/css" rel="stylesheet" href="../css/admin-reply_to_inquiry.css">
+</head>
+
+<body>
+    <?php
+    $formLink = $_SERVER['REQUEST_URI'];
+    $backLink = "/templates/admin-inquiry_details.php?inquiryId=" . $inquiryId;
+
+    echo <<<EOD
+    <form class="box" action="{$formLink}" method="post">
+        <h3>Reply to Inquiry</h3>
+        <textarea placeholder="Start writing reply..." name="replyText"></textarea>
+        <div>
+            <button class="btn reply">Reply to Inquiry</button>
+            <a class="btn cancel" href="{$backLink}">Cancel</a>
+        </div>
+    </form>
+    EOD;
+    ?>
+
+    
+
+</body>
+
 </html>
