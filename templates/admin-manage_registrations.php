eb2148cce88e9b6676a69caa0ed1603c1489dd61
eydriyan <115596606+eydriyan@users.noreply.github.com>
2024-05-05 23:47:19 +0800

    updated admin manage registrations UI
    updated admin manage registrations UI

--- templates/admin-manage_registrations.php
+++ templates/admin-manage_registrations.php
@@ -26,62 +26,82 @@
     <!-- Start Body -->
     <div class="main-body">
         <p>MANAGE REGISTRATIONS</p>
-
         <hr />
 
-        <?php
+        <div class="registrations-list-container">
 
-            // Check if the form is submitted
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                if (isset($_POST['confirm'])) {
-                    
-                    $studentId = $_POST['studentId'];
-                    // Confirm student registration
-                    StudentService::confirmStudentRegistration($studentId);
-                    header("Location: admin-manage_users.php");
-                }elseif (isset($_POST['reject'])) {
-                    $studentId = $_POST['studentId'];
-                    // Reject student registration
-                    StudentService::rejectStudentRegistration($studentId);
-                    
-                    echo "<script> alert('User Rejected');
-                    </script>";
+            <?php
+                // Check if the form is submitted
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if (isset($_POST['confirm'])) {
+                        
+                        $studentId = $_POST['studentId'];
+                        // Confirm student registration
+                        StudentService::confirmStudentRegistration($studentId);
+                        header("Location: admin-manage_users.php");
+                    }elseif (isset($_POST['reject'])) {
+                        $studentId = $_POST['studentId'];
+                        // Reject student registration
+                        StudentService::rejectStudentRegistration($studentId);
+                        
+                        echo "<script> alert('User Rejected');
+                        </script>";
 
+                    }
                 }
-            }
 
-            // Get all students
-            $students = StudentService::getAllStudents();
+                // Get all students
+                $students = StudentService::getAllStudents();
 
-            foreach ($students as $student) {
-                assert($student instanceof Student);
+                foreach ($students as $student) {
+                    assert($student instanceof Student);
 
-                // Check if the student is not verified
-                if (!$student->isVerified) {
-                    echo <<<EOD
+                    // Check if the student is not verified
+                    if (!$student->isVerified) {
+                        echo <<<EOD
 
-                    <div class="borrow-record-entry">
-                        <div>
-                            <span>{$student->StudNo}</span>
-                            <span>{$student->getFullName()}</span>
-                            <span>{$student->Email}</span>
-                            <span>{$student->Program}</span>
-                        </div>
-
-                        <form action="admin-manage_registrations.php" enctype="multipart/form-data" method="POST">
-                            <!-- Hidden input field to store student ID -->
-                            <input type="hidden" name="studentId" value="{$student->StudID}">
-                            <div class="button-container" text-center>
-                                <button type="submit" class="btn" name="confirm" value="confirm">Confirm</button>
-                                <button type="submit" class="btn" name="reject" value="reject">Reject</button>
+                        <div class="registration-record">
+                            <div>
+                                <div>
+                                    <span>Student No.</span>
+                                    <span>{$student->StudNo}</span>
+                                </div>
+                                <div>
+                                    <span>Full Name</span>
+                                    <span>{$student->getFullName()}</span>
+                                </div>
+                                <div>
+                                    <span>Email</span>
+                                    <span>{$student->Email}</span>
+                                </div>
+                                <div>
+                                    <span>Program</span>
+                                    <span>{$student->Program}</span>
+                                </div>
                             </div>
+
+
+                            <div>
+                                <form action="admin-manage_registrations.php" enctype="multipart/form-data" method="POST">
+                                    <!-- Hidden input field to store student ID -->
+                                    <input type="hidden" name="studentId" value="{$student->StudID}">
+                                        <button type="submit" class="btn" name="confirm" value="confirm">Confirm</button>
+                                        <button type="submit" class="btn" name="reject" value="reject">Reject</button>
+                                </form>
+                            </div>
+
                             
-                        </form>
-                    </div>
-                    EOD;
+
+                            
+                        </div>
+                        EOD;
+                    }
                 }
-            }
-        ?>
+            ?>
+
+        </div>
+
+        
     </div>
     
 </body>
