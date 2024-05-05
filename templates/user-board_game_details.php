4db8cb715aafda5a22892ae851d1bdbe9f85cab6
HansGan <90506270+HansGan@users.noreply.github.com>
2024-05-05 15:05:32 +0800

    updated board-game details

--- templates/user-board_game_details.php
+++ templates/user-board_game_details.php
@@ -21,6 +21,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/user-board_game_details.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
     <title>Board Game Details</title>
 </head>
 <body>
@@ -30,6 +35,7 @@
             require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
         ?>
 
+<<<<<<< Updated upstream
         <!--Form Start-->
         <form action="user-board_game_details.php" method="POST">
             <div class="main-container">
@@ -54,6 +60,82 @@
             </div>
         </form>
         <!--Content Start-->
+=======
+        <!--Content Start For non-members-->
+        <div class="main-container">
+            <div class="left-column">
+                <div class="img-container">
+                    <img class="img-styling" src="../assets/img-placeholder.png" alt="">
+                </div>
+                <div class="title-styling">
+                    Reviews
+                </div>
+                <div class="divider"></div>
+                <div class="reviews-container">
+                    <div class="review-content">
+                        <div class="author-styling">
+                            Author(Position)
+                        </div>
+                        <div class="testimony-review">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium praesentium nulla magnam magni quam similique fugit minus, tempore veritatis mollitia error dolore impedit atque. Nemo, provident non blanditiis eveniet vitae et facere ipsam a error dolorem ratione magni repudiandae reiciendis nesciunt dolore sunt obcaecati molestiae! Omnis labore, error ad placeat quasi nobis nisi! Id nam velit esse maxime neque, modi saepe veritatis, natus fugiat quia commodi, animi reiciendis. Adipisci, sunt quibusdam modi excepturi est soluta eaque, vero nobis eligendi architecto atque dolores voluptates accusamus sequi cumque quaerat voluptatibus corrupti. Officia reiciendis dolorum rerum inventore repellat hic earum expedita eveniet beatae.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right-column">
+                <div class="game-name-styling">
+                    Board Game Name
+                </div>
+                <div class="game-info">
+                    <div class="category-styling">
+                        Category
+                    </div>
+                    <div class="info-styling">
+                        Rent Price: <span class="value-styling">₱100.00</span>
+                    </div>
+                    <div class="info-styling">
+                        Rent Price: <span class="value-styling">5.0</span>
+                    </div>
+                </div>
+                <div class="title-styling">
+                    Overview
+                </div>
+                <div class="text-content">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, vel.
+                </div>
+                <div class="title-styling">
+                    How To Play
+                </div>
+                <div class="text-content">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, vel.
+                </div>
+                <div class="button-container">
+                    <button class="reserve-button">RESERVE THIS GAME</button>
+                    <button class="rent-button">RENT THIS GAME</button>
+                </div>
+                <div class="back-button-container">
+                    <button class="back-button">BACK</button>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!--
+>>>>>>> Stashed changes
         <div class="content-container">
             <?php
                         // Output events for the found game
@@ -71,6 +153,7 @@
                         echo '</div>';
             ?>
         </div>
+        -->
         
 </body>
 </html>
