8b83ef83934009cabcba9edf2aaabe9b5df557a1
HansGan <90506270+HansGan@users.noreply.github.com>
2024-05-09 00:10:05 +0800

    header-updated

--- components/header.php
+++ components/header.php
@@ -59,14 +59,12 @@ HEADER_STYLE_BOOTSTRAP;
     
                         <a href="../templates/user-manage_account.php">
                             <img style="width:36px;" src="../assets/gear.png" alt="">
-                        </a>
+                        </a>   
+                        
                         <div class="dropdown">
-                        <div class="btn btn-secondary dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                class="bi bi-bell mx-2" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
-                            </svg>
+                        <div class="btn btn-secondary dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style="position:relative;background-color:transparent;border:none;"> 
+                            <img style="width:36px;" src="../assets/bell.png" alt="">
+                            <div style="width:13px;height:13px;background-color:red;border-radius:100%;position:absolute;top:10px;right:30px"></div>
                         </div>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" id="notification-container">
                         </ul>
