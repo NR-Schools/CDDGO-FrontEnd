07dc4b1f9a22f3ce664b1985a8d84bc10fa24b70
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-05-09 00:46:33 +0800

    sign-in fixed

--- templates/sign-in.php
+++ templates/sign-in.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Perform Validation
     [$status, $error] = validate_many_inputs([
-        ["Email", $_POST['email'], [new MinLengthRule(1), new MaxLengthRule(50), new EmailRule(["@mymail.mapua.edu.ph"])]],
+        ["Email", $_POST['email'], [new MinLengthRule(1), new MaxLengthRule(50), new EmailRule(["@mymail.mapua.edu.ph", "@email.com"])]],
         ["Password", $_POST['password'], [new MinLengthRule(1), new MaxLengthRule(50)]]
     ]);
 
