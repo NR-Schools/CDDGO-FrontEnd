07d032ace4a79de41977c435e611c2dabcc6c8ae
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-05-09 07:22:43 +0800

    Fix reservation date unbounded issue

--- templates/user-reservation_details.php
+++ templates/user-reservation_details.php
@@ -1,72 +1,83 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/services/BoardGameService.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/services/AuthService.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/services/StudentService.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/services/ReservationService.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/guards/AuthGuard.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/services/BoardGameService.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/services/AuthService.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/services/StudentService.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/services/ReservationService.php");
 
-    if (!AuthGuard::guard_route(Role::USER)) {
-        // Return to root
-        header("Location: /");
-    }
+if (!AuthGuard::guard_route(Role::USER)) {
+    // Return to root
+    header("Location: /");
+}
 
 
-    
-    if ($_SERVER['REQUEST_METHOD'] === 'POST')
-    {
-        if(isset($_POST['reserve'])){
-            // Check if user is making a duplicate reservation
-            $gameID = $_POST["gameID"];
-            $game = BoardGameService::getBoardGameById($gameID);
-            $date = $_POST["date"];
-            $fee = 50;
 
-            // Get logged student
-            [$email, $role] = AuthService::getCurrentlyLoggedIn();
-            $currentUser = StudentService::getStudentByEmail($email);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['reserve'])) {
+        // Check if user is making a duplicate reservation
+        $gameID = $_POST["gameID"];
+        $game = BoardGameService::getBoardGameById($gameID);
+        $date = $_POST["date"];
+        $fee = 50;
 
-            $reservation = new Reservation();
-            $reservation->ReservationFee = $fee;
-            $reservation->ReservedDate = $date;
-            $reservation->createOnlyStudentId($currentUser->StudID);
-            $reservation->createOnlyBoardGameId($gameID);
-
-            [$result, $error] = ReservationService::addUserReservation($reservation);
-
-            if (!$result) {
-                // Reservation Failed!
-                echo "<script> alert('Reservation Failed');
-                document.location.href = 'user-board_games.php';
-                </script>";
-            }
-
-            // Reservation Success!
-            echo "<script> alert('Board Game Reserved');
-            document.location.href = 'user-board_games.php';
+        // Check if valid date
+        $currentDate = new DateTime();
+        $selectedDate = DateTime::createFromFormat('Y-m-d', $date);
+        $interval = $currentDate->diff($selectedDate);
+        $daysDifference = $interval->d;
+        if ($daysDifference < 0) {
+            echo "<script>
+                alert('Invalid Date for Reservation');
+                window.location.reload();
             </script>";
         }
 
-        if(isset($_POST['cancel']))
-        {
-            $id = $_POST["gameID"];
-            header("Location:../templates/user-board_game_details.php?gameId=". $id);
+        // Get logged student
+        [$email, $role] = AuthService::getCurrentlyLoggedIn();
+        $currentUser = StudentService::getStudentByEmail($email);
+
+        $reservation = new Reservation();
+        $reservation->ReservationFee = $fee;
+        $reservation->ReservedDate = $date;
+        $reservation->createOnlyStudentId($currentUser->StudID);
+        $reservation->createOnlyBoardGameId($gameID);
+
+        [$result, $error] = ReservationService::addUserReservation($reservation);
+
+        if (!$result) {
+            // Reservation Failed!
+            echo <<<EOD
+                <script> alert('Reservation Failed: {$error}');
+                    document.location.href = 'user-board_games.php';
+                </script>
+                EOD;
         }
+
+        // Reservation Success!
+        echo "<script> alert('Board Game Reserved');
+            document.location.href = 'user-board_games.php';
+            </script>";
     }
 
-    if(isset($_GET["gameId"]))
-    {
-        $gameID =(int)$_GET["gameId"];
-
-        $game = BoardGameService::getBoardGameById($gameID);
-        if ($game == null)
-        {
-            echo "No game.";
-        }
+    if (isset($_POST['cancel'])) {
+        $id = $_POST["gameID"];
+        header("Location:../templates/user-board_game_details.php?gameId=" . $id);
     }
+}
+
+if (isset($_GET["gameId"])) {
+    $gameID = (int) $_GET["gameId"];
+
+    $game = BoardGameService::getBoardGameById($gameID);
+    if ($game == null) {
+        echo "No game.";
+    }
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -77,10 +88,11 @@
         rel="stylesheet">
     <title>MTG - Reservation</title>
 </head>
+
 <body>
     <?php
-        require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php";
-        require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/components/header.php";
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/components/footer.php";
     ?>
     <form action="user-reservation_details.php" method="POST">
         <div class="main-container">
@@ -90,21 +102,22 @@
                 </div>
                 <div class="divider"></div>
 
-                <?php 
-                    echo '<div>';
-                    echo '<img class="game-image" src="data:image/' . pathinfo($game->GameImage, PATHINFO_EXTENSION) . ';base64,' . $game->GameImage . '" id="game_image">';
-                    echo '</div>';
-                    echo '<div class="name-styling" name="gameName" id="gameName">'.$game->GameName.'</div>';
-                    echo '<div class="price-styling" name="reservePrice" id="reservePrice">';
-                    echo  'Reservation Fee: ₱50.00';
-                    echo '</div>';
+                <?php
+                echo '<div>';
+                echo '<img class="game-image" src="data:image/' . pathinfo($game->GameImage, PATHINFO_EXTENSION) . ';base64,' . $game->GameImage . '" id="game_image">';
+                echo '</div>';
+                echo '<div class="name-styling" name="gameName" id="gameName">' . $game->GameName . '</div>';
+                echo '<div class="price-styling" name="reservePrice" id="reservePrice">';
+                echo 'Reservation Fee: ₱50.00';
+                echo '</div>';
                 ?>
                 <div class="date-container">
                     <div class="label-styling" name="date">
                         Select Date
                     </div>
                     <div>
-                        <input class="date-input" name="date" type="date">
+                        <input class="date-input" name="date" type="date"
+                            min="<?php echo date('Y-m-d', strtotime('+1 day', strtotime('tomorrow'))); ?>">
                     </div>
                 </div>
                 <div class="divider"></div>
@@ -117,4 +130,5 @@
         </div>
     </form>
 </body>
+
 </html>
