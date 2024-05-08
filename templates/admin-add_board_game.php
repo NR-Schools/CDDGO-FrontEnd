aba23138b22e421f50a42efbc751a95b5dbbf401
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-05-08 12:29:47 +0800

    Update validation settings for board games

--- templates/admin-add_board_game.php
+++ templates/admin-add_board_game.php
@@ -15,11 +15,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Perform Validation
     [$status, $error] = validate_many_inputs([
-        ["GameName", $_POST['game_name'], [new MinLengthRule(5), new MaxLengthRule(20)]],
-        ["GameDescription", $_POST['description'], [new MinLengthRule(5), new MaxLengthRule(20)]],
+        ["GameName", $_POST['game_name'], [new MinLengthRule(1), new MaxLengthRule(40)]],
+        ["GameDescription", $_POST['description'], [new MinLengthRule(1), new MaxLengthRule(400)]],
         ["GameImage", $_FILES['game_img'], [new ExistingFileRule()]],
         ["QuantityAvailable", $_POST['quantity_avail'], [new MinLengthRule(1)]],
-        ["GameCategory", $_POST['game_category'], [new MinLengthRule(5), new MaxLengthRule(20)]],
+        ["GameCategory", $_POST['game_category'], [new MinLengthRule(1), new MaxLengthRule(100)]],
     ]);
 
     echo <<<EOD
--- templates/admin-edit_board_game.php
+++ templates/admin-edit_board_game.php
@@ -18,10 +18,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // Perform Validation
         [$status, $error] = validate_many_inputs([
-            ["GameName", $_POST['game_name'], [new MinLengthRule(5), new MaxLengthRule(20)]],
-            ["GameDescription", $_POST['description'], [new MinLengthRule(5), new MaxLengthRule(20)]],
+            ["GameName", $_POST['game_name'], [new MinLengthRule(1), new MaxLengthRule(40)]],
+            ["GameDescription", $_POST['description'], [new MinLengthRule(1), new MaxLengthRule(400)]],
             ["QuantityAvailable", $_POST['quantity_avail'], [new MinLengthRule(1)]],
-            ["GameCategory", $_POST['game_category'], [new MinLengthRule(5), new MaxLengthRule(20)]],
+            ["GameCategory", $_POST['game_category'], [new MinLengthRule(1), new MaxLengthRule(100)]],
         ]);
 
         echo <<<EOD
